f9b4aca7ff7f64e887ed01849448b283b6cb2f5c
Thobani Blose <blstho005@myuct.ac.za>
2024-03-05 21:17:43 +0000

    Syntax

--- GenericsKbArrayApp.java
+++ GenericsKbArrayApp.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.IOExemption;
+import java.io.IOException;
 import java.util.Scanner;
 
 class Statement {
